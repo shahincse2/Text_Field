964da3f62f2a36adb9a24293b74f450e58ee1245
shahincse2 <shahinalam.cse2@gmail.com>
2024-07-31 20:45:45 +0600

    problem in theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'dart:js_interop';
-
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,23 +26,6 @@ class _MyAppState extends State<MyApp> {
           backgroundColor: Colors.blueGrey,
           centerTitle: true,
           foregroundColor: Colors.white, //foregroundColor: Colors.red,
-        ),
-        textTheme: TextTheme(
-          bodyLarge: GoogleFonts.acme(
-            fontSize: 28,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 0.5,
-          ),
-          bodyMedium: GoogleFonts.acme(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 0.4,
-          ),
-          bodySmall: GoogleFonts.acme(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 0.3,
-          ),
         ),
         inputDecorationTheme: InputDecorationTheme(
           focusedBorder: OutlineInputBorder(
@@ -90,90 +70,77 @@ class _MyAppState extends State<MyApp> {
             ),
           ),
         ),
-      ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-        scaffoldBackgroundColor: Colors.black,
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.blueGrey,
-          centerTitle: true,
-          //foregroundColor: Colors.red,
-        ),
-        textTheme: TextTheme(
-          bodyLarge: GoogleFonts.acme(
+
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(
             fontSize: 28,
             fontWeight: FontWeight.bold,
-            letterSpacing: 0.5,
-          ),
-          bodyMedium: GoogleFonts.acme(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 0.4,
-          ),
-          bodySmall: GoogleFonts.acme(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 0.3,
           ),
         ),
-        inputDecorationTheme: InputDecorationTheme(
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: const BorderSide(
-              color: Colors.green,
-              width: 2,
+      ),
+      darkTheme: ThemeData(
+          brightness: Brightness.dark,
+          scaffoldBackgroundColor: Colors.black,
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.blueGrey,
+            centerTitle: true,
+            //foregroundColor: Colors.red,
+          ),
+          inputDecorationTheme: InputDecorationTheme(
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: const BorderSide(
+                color: Colors.green,
+                width: 2,
+              ),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(
+                color: Colors.blue.shade900,
+                width: 2,
+              ),
+            ),
+            errorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(
+                color: Colors.red.shade900,
+                width: 2,
+              ),
+            ),
+            hintStyle: const TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+            ),
+            labelStyle: const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
             ),
           ),
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(
-              color: Colors.blue.shade900,
-              width: 2,
-            ),
-          ),
-          errorBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(
-              color: Colors.red.shade900,
-              width: 2,
-            ),
-          ),
-          hintStyle: const TextStyle(
-            color: Colors.white,
-            fontSize: 16,
-          ),
-          labelStyle: const TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ButtonStyle(
-            backgroundColor: WidgetStateProperty.all(Colors.blueGrey),
-            foregroundColor: WidgetStateProperty.all(Colors.white),
-            //elevation: WidgetStateProperty.all(10),
-            shape: WidgetStateProperty.all(
-              RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ButtonStyle(
+              backgroundColor: WidgetStateProperty.all(Colors.blueGrey),
+              foregroundColor: WidgetStateProperty.all(Colors.white),
+              //elevation: WidgetStateProperty.all(10),
+              shape: WidgetStateProperty.all(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
               ),
             ),
           ),
-        ),
-        textButtonTheme: TextButtonThemeData(
-          style: TextButton.styleFrom(
-            textStyle: GoogleFonts.laila(
-              fontSize: 28,
-              fontWeight: FontWeight.bold,
+          textTheme: const TextTheme(
+            bodyLarge: TextStyle(
+              color: Colors.white,
             ),
-          )
-        ),
-      ),
+          )),
       themeMode: _themeMode ? ThemeMode.light : ThemeMode.dark,
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: const Text(
             'Text Field',
+            //style: Theme.of(context).textTheme.bodyLarge,
           ),
           actions: [
             Switch(
@@ -190,94 +157,92 @@ class _MyAppState extends State<MyApp> {
             )
           ],
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Padding(
-              padding: EdgeInsets.all(16.0),
-              child: TextField(
-                enabled: true,
-                decoration: InputDecoration(
-                  hintText: 'Enter your email',
-                  labelText: 'Email',
-                  prefixIcon: Icon(Icons.email_outlined),
+        body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(16.0),
+                child: TextField(
+                  enabled: true,
+                  decoration: InputDecoration(
+                    hintText: 'Enter your email',
+                    labelText: 'Email',
+                    prefixIcon: Icon(Icons.email_outlined),
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextField(
-                enabled: true,
-                decoration: InputDecoration(
-                  hintText: 'Enter your password',
-                  labelText: 'Password',
-                  prefixIcon: const Icon(Icons.lock_clock_outlined),
-                  suffixIcon: IconButton(
-                      onPressed: () {
-                        setState(() {
-                          _securedType = !_securedType;
-                        });
-                      },
-                      icon: Icon(_securedType
-                          ? Icons.visibility_off_outlined
-                          : Icons.visibility)),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+                  enabled: true,
+                  decoration: InputDecoration(
+                    hintText: 'Enter your password',
+                    labelText: 'Password',
+                    prefixIcon: const Icon(Icons.lock_clock_outlined),
+                    suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            _securedType = !_securedType;
+                          });
+                        },
+                        icon: Icon(_securedType
+                            ? Icons.visibility_off_outlined
+                            : Icons.visibility)),
+                  ),
+                  obscureText: _securedType,
                 ),
-                obscureText: _securedType,
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextField(
-                enabled: true,
-                controller: _descriptionTEcontroller,
-                maxLines: 3,
-                maxLength: 200,
-                decoration: const InputDecoration(
-                  hintText: 'Write Description',
-                  labelText: 'Description',
-                ),
-                onTap: () {
-                  print('OnTaped');
-                },
-                onChanged: (String value) {
-                  print(value);
-                },
-                //textAlign: TextAlign.right,
-                textAlignVertical: TextAlignVertical.top,
-              ),
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('Submit'),
-                ),
-                SizedBox(
-                  width: 25,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    // _descriptionTEcontroller.text = 'Description';
-                    _descriptionTEcontroller.clear();
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+                  enabled: true,
+                  controller: _descriptionTEcontroller,
+                  maxLines: 3,
+                  maxLength: 200,
+                  decoration: const InputDecoration(
+                    hintText: 'Write Description',
+                    labelText: 'Description',
+                  ),
+                  onTap: () {
+                    print('OnTaped');
                   },
-                  child: const Text('Clear'),
+                  onChanged: (String value) {
+                    print(value);
+                  },
+                  //textAlign: TextAlign.right,
+                  textAlignVertical: TextAlignVertical.top,
                 ),
-              ],
-            ),
-            TextButton(
-              onPressed: () {},
-              child: Text(
-                "I'm a text button in this world...!",
-                style: Theme.of(context).textTheme.bodyLarge,
               ),
-            )
-          ],
+              const SizedBox(
+                height: 16,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Submit'),
+                  ),
+                  const SizedBox(
+                    width: 25,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      // _descriptionTEcontroller.text = 'Description';
+                      _descriptionTEcontroller.clear();
+                    },
+                    child: const Text('Clear'),
+                  ),
+                ],
+              ),
+              Text("I'm a text in this world...!",
+                  style: Theme.of(context).textTheme.bodyLarge),
+            ],
+          ),
         ),
       ),
     );
